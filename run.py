bcaed9639cc05bddd885ddbd64c217c8f1fe8c05
RYDAHXYBER TEAM <111449068+RydahXyber@users.noreply.github.com>
2022-09-21 17:31:43 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os, platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from run64 import sup
+    from run32 import sup
     sup()
 elif bit == '32bit':
     from run32 import sup

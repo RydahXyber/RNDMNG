f1cc713f443e05d81f2271417870710d4a953276
RYDAHXYBER TEAM <111449068+RydahXyber@users.noreply.github.com>
2022-09-21 17:32:41 +0100

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import os, platform
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from run32 import sup
-    sup()
-elif bit == '32bit':
-    from run32 import sup
-    sup()
